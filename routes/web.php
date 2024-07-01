5ff17a411f920e6acf5f8894fdbf9e45ce2449b5
Masmat97 <matteo.mascio@libero.it>
2024-07-01 15:41:37 +0200

    Capitolo 1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,18 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/', function () {
+    return view('cards');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/home', function () {
+    return view('home');
 });
